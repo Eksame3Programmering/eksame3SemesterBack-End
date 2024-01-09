3afcd83ea3a1d1f332ea3a9916f190f670052a1f
cihadozdemir <ciha0128@gmail.com>
2024-01-09 16:24:23 +0100

    Initial commit

---
+++ src/main/java/com/example/eksame3semesterbackend/Eksame3SemesterBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.eksame3semesterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eksame3SemesterBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eksame3SemesterBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eksame3semesterbackend/Eksame3SemesterBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eksame3semesterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eksame3SemesterBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

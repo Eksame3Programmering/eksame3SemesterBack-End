5b2560ebd1a95ccfcd7973ddd0e2a334a8aa5d60
cihadozdemir <ciha0128@gmail.com>
2024-01-12 06:29:54 +0100

    cihad123

--- src/test/java/com/example/eksame3semesterbackend/RoomServiceTest.java
+++ src/test/java/com/example/eksame3semesterbackend/RoomServiceTest.java
@@ -6,7 +6,6 @@ import com.example.eksame3semesterbackend.service.RoomService;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
@@ -14,10 +13,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.hamcrest.Matchers.any;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
-import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
 
 @SpringBootTest
 
@@ -30,17 +27,14 @@ public class RoomServiceTest {
 
     @Test
     void getAllRooms() {
-        // Arrange
         Long hotelId = 1L;
         when(roomRepository.findAllRoomsInHotel(hotelId)).thenReturn(Arrays.asList(
                 new Room(1L, "101", 2, 100, null, null, LocalDateTime.now(), LocalDateTime.now()),
                 new Room(2L, "202", 1, 150, null, null, LocalDateTime.now(), LocalDateTime.now())
         ));
 
-        // Act
         List<Room> result = roomService.findAllRoomsInHotel(hotelId);
 
-        // Assert
         assertEquals(2, result.size());
     }
 
@@ -50,14 +44,11 @@ public class RoomServiceTest {
 
     @Test
     void getRoomById() {
-        // Arrange
         Long id = 1L;
         when(roomRepository.findById(id)).thenReturn(Optional.of(new Room(id, "101", 2, 100, null, null, LocalDateTime.now(), LocalDateTime.now())));
 
-        // Act
         Optional<Room> result = roomService.getRoomById(id);
 
-        // Assert
         assertTrue(result.isPresent());
         assertEquals(Math.toIntExact(id), Math.toIntExact(result.get().getId()));
     }
@@ -70,31 +61,24 @@ public class RoomServiceTest {
 
     @Test
     void updateRoom() {
-        // Arrange
         Long id = 1L;
         Room existingRoom = new Room(id, "101", 2, 100, null, null, LocalDateTime.now(), LocalDateTime.now());
         Room updatedRoom = new Room(id, "101_updated", 3, 150, null, null, LocalDateTime.now(), LocalDateTime.now());
 
         when(roomRepository.findById(id)).thenReturn(Optional.of(existingRoom));
 
-        // Act
         Room result = roomService.updateRoom(id, updatedRoom);
 
-        // Assert
         assertEquals(updatedRoom.getRoomNumber(), result.getRoomNumber());
         assertEquals(updatedRoom.getNumberOfBeds(), result.getNumberOfBeds());
-        // Add more assertions based on your requirements
     }
 
     @Test
     void deleteRoom() {
-        // Arrange
         Long id = 1L;
 
-        // Act
         roomService.deleteRoom(id);
 
-        // Assert
         verify(roomRepository, times(1)).deleteById(id);
     }
 
